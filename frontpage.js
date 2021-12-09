6e1050e5d60a19a995f72f112ee590574ac55ecd
AnirudhSrivastava2425 <91976754+AnirudhSrivastava2425@users.noreply.github.com>
2021-12-09 14:25:38 +0530

    Add files via upload

---
+++ frontpage.js
@@ -0,0 +1,10 @@
+function cls() {
+    var dis = document.getElementById("menubar");
+    if (dis.style.display=="none") {
+        dis.style.display="flex";
+    }
+    else
+    {
+        dis.style.display="none";
+    }
+}
